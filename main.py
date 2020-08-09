67b5982078ee8e6f3ffb28ae6c1a6b64bed6e9f1
Bryan Dady <bryan@subsplash.com>
2020-08-09 00:03:15 -0600

    provide main.py template

---
+++ main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import sys
+import os
+import csv
+
+# import csv data
+
+# make sure nobody voted more than once
+    
+# total # of votes cast
+
+# list of counties
+
+# list of candidates
+
+# count votes -- for each candidate and county
+    
+# Election Results
+
+# Total Votes
+
+# Votes by county
+
+# Votes by candidate
+
+# Declare a Winner
+
+# print results to stdout
+
+# print results to file
